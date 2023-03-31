89dfa7024b9e9912b36192b90eac8b6e82257036
Christiaan Dames <36244641+HUSKUT@users.noreply.github.com>
2023-03-31 11:47:44 +0200

    im an idiot.exe

--- routes/index.js
+++ routes/index.js
@@ -12,43 +12,45 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 let html;
 
 async function generateCat() {
-    fetch('https://api.thecatapi.com/v1/images/search?size=full').then((response) => response.json())
+    await fetch('https://api.thecatapi.com/v1/images/search?size=full').then((response) => response.json())
         .then((data) => {
             console.log(data[0].url)
-            html = '<img src="' + data[0].url + '">';
+            html = '<h1>Dagelijkse kat</h1>'
+            html += '<img src="' + data[0].url + '">';
         });
+
+    return html;
 }
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
 router.post('/kim/send', async function (req, res) {
     console.log(req.body.token)
     if (req.body.token === process.env.MAIL_TOKEN) {
         await generateCat();
+        console.log('html', html)
         const msg = {
             to: process.env.SENDGRID_TO_EMAIL,
             from: {
                 email: process.env.SENDGRID_FROM_EMAIL
             },
             subject: 'Dagelijkse kat',
-            text: 'Dagelijkse kat',
             html: html,
         }
 
         res.send('Hello World');
 
-        console.log(process.env.SENDGRID_TO_EMAIL)
         sgMail
             .send(msg)
             .then((response) => {
-                // console.log(response[0].statusCode)
+                console.log('response', response[0].statusCode)
                 // console.log(response[0].headers)
             })
             .catch((error) => {
-                console.error(error.response.body)
+                console.error(error)
             })
     } else {
         res.send('Invalid token, doei.');
