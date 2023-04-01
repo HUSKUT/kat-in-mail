cbed1d4ee1a98621889b3ef38c5fb6ed10721ce2
Christiaan Dames <36244641+HUSKUT@users.noreply.github.com>
2023-04-01 11:40:43 +0200

    hopeful la fix for beepbeepboopboop code

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
     "type": process.env.FIREBASE_ACC_TYPE,
     "project_id": process.env.FIREBASE_PROJECT_ID,
     "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.split(String.raw`\n`).join('\n')
         ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n")
         : undefined,
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
