efc1822b9454102ca7daa9741e6d7bda2c02d597
Christiaan Dames <36244641+HUSKUT@users.noreply.github.com>
2023-04-01 18:19:43 +0200

    :poop: hotfix

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 let mails = [];
 
 
-    const firebaseConfig = process.env.FIREBASE_CONFIG ?? firebase.credential.cert({
+    const firebaseConfig =  !!process.env.FIREBASE_CONFIG ? firebase.credential.cert(process.env.FIREBASE_CONFIG) : firebase.credential.cert({
         "type": process.env.FIREBASE_ACC_TYPE,
         "project_id": process.env.FIREBASE_PROJECT_ID,
         "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
