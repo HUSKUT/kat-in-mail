c65c1f9a5a85d47ecbc1693a28baf76cc45eb3d0
Christiaan Dames <36244641+HUSKUT@users.noreply.github.com>
2023-04-06 17:30:50 +0200

    added unsubscribing

--- routes/index.js
+++ routes/index.js
@@ -4,80 +4,83 @@ const fetch = require("node-fetch");
 const firebase = require("firebase-admin");
 const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-
 const dotenv = require('dotenv');
 const {applicationDefault} = require("firebase-admin/app");
+const mail = require("@sendgrid/mail");
 dotenv.config();
 let mails = [];
+let catimg;
 
+async function generateCat() {
+    await fetch('https://api.thecatapi.com/v1/images/search?size=full').then((response) => response.json())
+        .then((data) => {
+            console.log(data[0].url)
+            catimg = data[0].url;
+        });
+}
 
+catimg = generateCat();
 async function initFirebase() {
 // Initialize Firebase
-
-
     const app = firebase.initializeApp({
         credential: applicationDefault(),
         databaseURL: process.env.FIRESTORE_DATABASE_URL
     });
 // Initialize Cloud Firestore and get a reference to the service
-
     const store = firebase.firestore(app);
     try {
-        await store.collection("/users").get().then((querySnapshot) => {
+        await store.collection("/users").get().then(async (querySnapshot) => {
+
             querySnapshot.forEach((doc) => {
-                mails.push(doc.data().email);
+                mails.push(
+                    {
+                        from: {
+                            name: 'Kat in Mail',
+                            email: process.env.SENDGRID_FROM_EMAIL
+                        },
+                        template_id: process.env.SENDGRID_TEMPLATE_ID,
+                        subject: 'Dagelijkse kat',
+                        personalizations: [
+                            {
+                                to: [doc.data().email],
+                                subject: 'Dagelijkse kat',
+                                dynamic_template_data: {
+                                    USERID: doc.id,
+                                    CATIMAGE: catimg
+                                }
+                            }],
+                    }
+                );
             });
         });
     } catch (e) {
         console.error("Error getting document: ", e);
     }
 }
-
 initFirebase().then(r => console.log('connection made'));
-
-let html;
-
-async function generateCat() {
-    await fetch('https://api.thecatapi.com/v1/images/search?size=full').then((response) => response.json())
-        .then((data) => {
-            console.log(data[0].url)
-            html = '<h1>Dagelijkse kat</h1>'
-            html += '<img src="' + data[0].url + '">';
-        });
-    return html;
-}
-
-/* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
 
+router.get('/kim/unsubscribe', function (req, res, next) {
+    if (req.query.user == null) return res.send('No user specified');
+    firebase.firestore().collection('users').doc(req.query.user).delete().then(() => {
+        res.send('Unsubscribed!');
+    }).catch((e) => {
+        res.send('Error unsubscribing: ' + e);
+    });
+});
+
 router.post('/kim/send', async function (req, res) {
     if (req.body.token === process.env.MAIL_TOKEN) {
-        await generateCat();
-        console.log('html', html)
-        const msg = {
-            to: mails,
-            from: {
-                name: 'Kat in Mail',
-                email: process.env.SENDGRID_FROM_EMAIL
-            },
-            subject: 'Dagelijkse kat',
-            html: html,
-        }
-
         res.send('Sent mails!');
         sgMail
-            .sendMultiple(msg)
+            .sendMultiple(mails)
             .then((response) => {
-                console.log('response', response[0].statusCode)
-                // console.log(response[0].headers)
+                console.log('response', response)
             })
             .catch((error) => {
-                console.error(error)
-                for (let i = 0; i < error.body.length; i++) {
-                    console.log(error.body[i])
-                }
+                console.log('error', error.response.body.errors)
             })
     } else {
         res.send('Invalid token, doei.');
